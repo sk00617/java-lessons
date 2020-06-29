053b318cbaedee187d92de169077c15c10b17f75
Karina <67572979+sk00617@users.noreply.github.com>
2020-06-29 00:14:55 -0500

    Added palindrome exercise

---
+++ Palindrome/src/palindrome/Palindrome.java
@@ -0,0 +1,14 @@
+package palindrome;
+
+public class Palindrome {
+	public boolean isPalindrome(String input){
+		input=input.replaceAll(" ","");
+		input=input.toLowerCase();
+		int n = input.length();
+	    for( int i = 0; i <= n/2; i++ )
+	        if (input.charAt(i) != input.charAt(n-i-1))
+	        	return false;
+	    return true;  
+	}
+
+}
