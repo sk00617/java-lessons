dfd09b4decdde23aba9453051796cee2f7c86819
Karina <67572979+sk00617@users.noreply.github.com>
2020-08-03 18:03:32 -0500

    Added Telegrama.java

---
+++ Telegrama/src/telegrama/Telegrama.java
@@ -0,0 +1,22 @@
+package telegrama;
+
+public class Telegrama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mensaje="Tipos de mamíferos voladores según su vuelo";
+		mensaje=mensaje.toLowerCase();
+		mensaje=mensaje.replace(" ", "");
+		int costo=0;
+		for (char c:mensaje.toCharArray()) {
+			if (c>=48 && c<=57)
+				costo+=20;
+			if (c>=97 && c<=122)
+				costo+=10;
+			else
+				costo+=30;
+		}
+		System.out.println(costo);
+	}
+
+}
